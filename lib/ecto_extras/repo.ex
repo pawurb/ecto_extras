bb5cd4d94eaee8c17c155b32350af14005ff1cb1
pawurb <contact@pawelurbanek.com>
2021-01-13 11:21:27 +0100

    Improve docs

--- lib/ecto_extras/repo.ex
+++ lib/ecto_extras/repo.ex
@@ -14,66 +14,42 @@ defmodule EctoExtras.Repo do
         queryable |> Ecto.Query.first(order_by) |> one()
       end
 
-      @doc """
-      The same as `first` but raises Ecto.NoResultsError if object is not found.
-      """
       def first!(queryable, order_by \\ nil)
 
       def first!(queryable, order_by) do
         queryable |> Ecto.Query.first(order_by) |> one!()
       end
 
-      @doc """
-      Returns last object from the query result. By default ordered by the primary key.
-      """
       def last(queryable, order_by \\ nil)
 
       def last(queryable, order_by) do
         queryable |> Ecto.Query.last(order_by) |> one()
       end
 
-      @doc """
-      The same as `last` but raises Ecto.NoResultsError if object is not found.
-      """
       def last!(queryable, order_by \\ nil)
 
       def last!(queryable, order_by) do
         queryable |> Ecto.Query.last(order_by) |> one!()
       end
 
-      @doc """
-      Returns count of objects from the query result.
-      """
       def count(queryable, column \\ :id)
 
       def count(queryable, column) do
         aggregate(queryable, :count, column)
       end
 
-      @doc """
-      Returns the maximum value of a column from the query result.
-      """
-      def max(queryable, column) do
-        aggregate(queryable, :max, column)
-      end
-
-      @doc """
-      Returns the minimum value of a column from the query result.
-      """
       def min(queryable, column) do
         aggregate(queryable, :min, column)
       end
 
-      @doc """
-      Returns the average value of a column from the query result.
-      """
+      def max(queryable, column) do
+        aggregate(queryable, :max, column)
+      end
+
       def avg(queryable, column) do
         aggregate(queryable, :avg, column)
       end
 
-      @doc """
-      Returns the sum of column values from the query result.
-      """
       def sum(queryable, column) do
         aggregate(queryable, :sum, column)
       end
@@ -83,5 +59,46 @@ defmodule EctoExtras.Repo do
   @doc """
   Returns first object from the query result. By default ordered by the primary key.
   """
-  @callback first(query :: Ecto.Queryable.t(), params :: keyword()) :: Ecto.Queryable.t()
+  @callback first(query :: Ecto.Queryable.t(), column :: atom) :: Ecto.Schema.t() | nil
+
+  @doc """
+  The same as `first` but raises Ecto.NoResultsError if object is not found.
+  """
+  @callback first!(query :: Ecto.Queryable.t(), column :: atom) :: Ecto.Schema.t()
+
+  @doc """
+  Returns last object from the query result. By default ordered by the primary key.
+  """
+  @callback last(query :: Ecto.Queryable.t(), column :: atom) :: Ecto.Schema.t() | nil
+
+  @doc """
+  The same as `last` but raises Ecto.NoResultsError if object is not found.
+  """
+  @callback last!(query :: Ecto.Queryable.t(), column :: :id) :: Ecto.Schema.t()
+
+  @doc """
+  Returns count of objects from the query result.
+  """
+  @callback count(query :: Ecto.Queryable.t(), column :: atom) :: term | nil
+
+  @doc """
+  Returns the minimum value of a column from the query result.
+  """
+  @callback min(query :: Ecto.Queryable.t(), column :: atom) :: term | nil
+
+  @doc """
+  Returns the maximum value of a column from the query result.
+  """
+  @callback max(query :: Ecto.Queryable.t(), column :: atom) :: term | nil
+
+  @doc """
+  Returns the average value of a column from the query result.
+  """
+  @callback avg(query :: Ecto.Queryable.t(), column :: atom) :: term | nil
+
+  @doc """
+  Returns the sum of column values from the query result.
+  """
+  @callback sum(query :: Ecto.Queryable.t(), column :: atom) :: term | nil
+
 end
